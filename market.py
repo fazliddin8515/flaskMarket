e0b01464267c2e0791eddf0b95e44f45b3838df7
Fazliddin O'sarov <fazliddinusarov1999@gamil.com>
2025-02-04 16:10:10 +0500

    setup flask-sqlalchemy

--- market.py
+++ market.py
@@ -1,6 +1,29 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+
+
+class Base(DeclarativeBase):
+    pass
+
+
+db = SQLAlchemy(model_class=Base)
 
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///market.sqlite"
+
+db.init_app(app)
+
+
+class Item(db.Model):  # type: ignore
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    name: Mapped[str] = mapped_column(nullable=False)
+    barcode: Mapped[int] = mapped_column(nullable=False, unique=True)
+    price: Mapped[int] = mapped_column(nullable=False)
+
+
+with app.app_context():
+    db.create_all()
 
 
 @app.route("/")
